7a6c602a958c7b05b20cec28f69c9b6deac52b82
JelenaTomic <tomic.elena09@gmail.com>
2022-08-26 18:31:58 -0500

    added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+USE employee_db;
+
+CREATE TABLE department (
+  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL, 
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  
+
+);
+
+CREATE TABLE employee (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER NOT NULL, 
+  manager_id INTEGER,
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id),
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+
+);
