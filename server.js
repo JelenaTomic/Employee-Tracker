5770f8c75d97e8ded15cc9609731cd0aa10e8683
JelenaTomic <tomic.elena09@gmail.com>
2022-08-26 19:23:31 -0500

    starting server after connecting to DB

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ const inquirer = require("inquirer");
 const db = require('./connect');
 
 const consoleTable = require('console.table');
+
+//start server
+db.connect(error => {
+    if(error) throw error;
+    console.log('Database connected');
+    Start();
+    getDepartments();
+    getRoles();
+    getEmployees();
+});
