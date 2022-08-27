bffc96ab006d84dd3c7b5277437b3232dbeebc7f
JelenaTomic <tomic.elena09@gmail.com>
2022-08-26 19:34:07 -0500

    added start function

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const db = require('./connect');
 
 const consoleTable = require('console.table');
+const { addListener } = require("./connect");
 
 //start server
 db.connect(error => {
@@ -12,3 +13,33 @@ db.connect(error => {
     getRoles();
     getEmployees();
 });
+
+function start() {
+    inquirer.prompt({
+      type : "list",
+      name: "choices",
+      message : "What would you like to do?",
+      choices : [ "View" , "Update" , "Add" , "Delete" , "Exit"]
+    }).then(answer =>{
+        switch(answer.choices){
+            case "View":
+                views();
+            break;
+            case "Update":
+                updates();
+             break;
+            case "Add":
+                adds();
+            break;
+            case "Delete":
+                deletes();
+            break;
+            case "Exit":
+                db.end();
+            break;
+        }
+    });
+};
+
+        
+    
